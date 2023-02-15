a3308e8534447c2d8faeae6614a2f6f9d10c3432
hentype85 <martinleiro9@gmail.com>
2023-02-15 13:02:04 -0800

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,10 +4,9 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- */					
+ */
 int main(void)
 {
-	char *p = "\"Programming is like building a multilingual puzzle";
-	puts(p);
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
