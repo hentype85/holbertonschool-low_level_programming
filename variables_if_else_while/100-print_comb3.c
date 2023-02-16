e613fb66ec7c3d137c6f66e69cc879deb4df7f6e
hentype85 <martinleiro9@gmail.com>
2023-02-16 15:40:56 -0800

    comb3

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -17,9 +17,11 @@ int main(void)
 			putchar((i % 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i <= 8 && j < 9)
+			if (i < 8 && j <= 9)
 			{
+				if (j < 9)
 				putchar(',');
+
 				putchar(' ');
 			}
 		}
