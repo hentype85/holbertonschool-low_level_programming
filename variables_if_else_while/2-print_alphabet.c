33949dd8f6d2daf80c8efa52f3114216e560fd46
hentype85 <martinleiro9@gmail.com>
2023-02-16 08:59:12 -0800

    printAZ

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char L;
+
+	for (L = 'a'; L <= 'z'; L++)
+		putchar(L);
+	putchar('\n');
+	return (0);
+}
