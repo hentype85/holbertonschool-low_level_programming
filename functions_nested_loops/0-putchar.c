9c4ae5e45a82670508cfb4da73b0abd3e19fb83e
hentype85 <martinleiro9@gmail.com>
2023-02-17 06:27:32 -0800

    putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char array[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7 ; i++)
+	{
+		_putchar(array[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
