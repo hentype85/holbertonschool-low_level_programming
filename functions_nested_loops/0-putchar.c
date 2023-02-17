e8edbb7e9004794613d7b36f7ef2cf09a3df5b28
hentype85 <martinleiro9@gmail.com>
2023-02-17 06:55:10 -0800

    putch

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-	char array[] = "_putchar";
-	int i;
-
-	for (i = 0; i <= 7 ; i++)
-	{
-		_putchar(array[i]);
-	}
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
