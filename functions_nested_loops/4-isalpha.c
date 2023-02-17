a41bbb6f07cc3715c8bda26f5ec2bfc46eac62ad
hentype85 <martinleiro9@gmail.com>
2023-02-17 12:58:13 -0800

    alpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * _isalpha - a function that check for alphabetic character
  * @c: single letter input
  * Returns 1 if c is a letter, lowercase or uppercase, returns 0 otherwise
