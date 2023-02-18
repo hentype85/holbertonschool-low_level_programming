63a79d0e41cd5414a4cf49a2cd2bce86f08d9c30
hentype85 <martinleiro9@gmail.com>
2023-02-18 05:17:32 -0800

    24hs

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,7 +14,12 @@ void jack_bauer(void)
 	{
 		for (m = 0; m < 60; m++)
 		{
-			printf("%02d:%02d\n", h, m);
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
