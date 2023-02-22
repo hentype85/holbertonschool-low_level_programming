b19f95dc3640c31eb92f283507bf93a82255e220
hentype85 <martinleiro9@gmail.com>
2023-02-21 18:11:41 -0800

    fix

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -12,28 +12,30 @@ void print_times_table(int n)
 	{
 		for (i = 0 ; i <= n ; i++)
 		{
-			_putchar('0');
-
 			for (j = 0 ; j <= n ; j++)
 			{
 				res = i * j;
-				_putchar(',');
-				_putchar(' ');
-				if (n <= 9)
+				if (n <= 9 && j != 0)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(res + '0');
 				}
-				else if (n <= 99)
+				else if (n <= 99 && n >= 10)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(res / 10 + '0');
 					_putchar(res % 10 + '0');
 				}
-				else
+				else if (n >= 100)
 				{
-					_putchar((res / 100) % 10 + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar((res / 100) + '0');
 					_putchar((res / 10) % 10 + '0');
 					_putchar(res % 10 + '0');
 				}
