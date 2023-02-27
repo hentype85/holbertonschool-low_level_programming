635ac9303baa24094f864c678923d7f861f1461b
hentype85 <martinleiro9@gmail.com>
2023-02-27 15:17:07 -0800

    revString

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -6,21 +6,16 @@
  */
 void rev_string(char *s)
 {
-	int i = 0, len = 0, half = 0;
-	char temp;
+	int i = 0, len = 0;
+	char tmp;
 
-	while (s[i])
-	{
-		i++;
+	while (s[i++])
 		len++;
-	}
 
-	half = len / 2;
-
-	for (i = len - 1; i >= half; i++)
+	for (i = len - 1; i >= len / 2; i--)
 	{
-		temp = s[i];
+		tmp = s[i];
 		s[i] = s[len - i - 1];
-		s[len - i - 1] = temp;
+		s[len - i - 1] = tmp;
 	}
 }
