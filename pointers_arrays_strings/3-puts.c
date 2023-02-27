1b0794a4d428ad186522211e4fb42e515ac1b6a2
hentype85 <martinleiro9@gmail.com>
2023-02-27 08:34:08 -0800

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
-/*
- *_puts: prints a string, followed by a new line, to stdout
- *@str: string parameter
+/**
+ * _puts - Prints a string.
+ * @str: string.
  */
 
 void _puts(char *str)
