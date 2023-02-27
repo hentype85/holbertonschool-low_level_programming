2013da94dd2e7ae7650580c606069ec930be3671
hentype85 <martinleiro9@gmail.com>
2023-02-27 12:48:38 -0800

    revString

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string.
+ * @s: string to be reserved.
+ */
+void rev_string(char *s)
+{
+	int i = 0, j = 0;
+	char temp;
+
+	while (s[i])
+	{
+		i++;
+		j++;
+	}
+
+	for (i = 0; i <= j / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[j - i - 1];
+		s[j - i - 1] = temp;
+	}
+}
