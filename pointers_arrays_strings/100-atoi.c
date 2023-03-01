ede4d825adff4a9190d4adf86d496009c960aa43
hentype85 <martinleiro9@gmail.com>
2023-03-01 05:58:35 -0800

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,19 @@
  */
 int _atoi(char *s)
 {
-	unsigned int res, i = 0, len = 0, sign = 1;
+	unsigned int res, len = 0, sign = 1;
 
-	while (s[i])
+	do
 	{
-		len++;
-		i++;
-	}
-
-	for (i = 0 ; i < len ; i++)
-	{
-		if (s[i] == '-')
+		if (*s == '-')
 			sign *= -1;
 
-		else if (s[i] >= '0' && s[i] <= '9')
-			res = (res * 10) + (s[i] - '0');
+		else if (*s >= '0' && *s <= '9')
+			res = (res * 10) + (*s - '0');
 
 		else if (res > 0)
 			break;
-	}
+	} while (*s++);
+
 	return (res * sign);
 }
