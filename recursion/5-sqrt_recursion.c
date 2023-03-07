1860f9a1a471e30f09ab96fb6d0a5729153c4780
hentype85 <martinleiro9@gmail.com>
2023-03-07 08:14:34 -0800

    sqrtRec

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ *compare - returns the power of number
+ *@n: parameter number
+ *@i: counter to compare multiplication
+ *
+ *Return: square root
+ */
+
+int compare(int n, int i)
+{
+	if (n == i * i)
+		return (i);
+	else if (n < i * i)
+		return (-1);
+
+	return (compare(n, i + 1));
+}
+/*
+ *_sqrt_recursion - returns the power of number
+ *@n: parameter number
+ *Return: natural square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (compare(n, 1));
+}
