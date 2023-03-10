4560ceb197c55de9ac6933d958b7290467d5f450
hentype85 <martinleiro9@gmail.com>
2023-03-10 12:50:16 -0800

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 
 /**
- *
  * main - add arguments
  * @argc: argument count
  * @argv: argument matrix for values
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	int i = 0, j = 0, res = 0;
 
-	for( i = 1 ; i < argc ; i++)
+	for (i = 1 ; i < argc ; i++)
 	{
-		for(j = 0 ; argv[i][j] != '\0' ; j++)
+		for (j = 0 ; argv[i][j] != '\0' ; j++)
 		{
 			if ((argv[i][j] < 47) || (argv[i][j] > 57))
 			{
 				printf("Error\n");
-				return(1);
+				return (1);
 			}
 		}
 
