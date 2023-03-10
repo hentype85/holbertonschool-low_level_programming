5a40898fa0ca731f838c39561c123205f134c410
hentype85 <martinleiro9@gmail.com>
2023-03-10 12:23:16 -0800

    add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ * main - add arguments
+ * @argc: argument count
+ * @argv: argument matrix for values
+ */
+int main(int argc, char *argv[])
+{
+	int i = 0, j = 0, res = 0;
+
+	for( i = 0 ; i < argc ; i++)
+	{
+		for(j = 0 ; argv[i][j] != '\0' ; j++)
+		{
+			if (argv[i][j] < 47  || argv[i][j] > 57)
+			{
+				printf("Error\n");
+				return(1);
+			}
+		}
+
+		res += atoi(argv[i]);
+	}
+
+	printf("%d\n", res);
+	return (0);
+}
