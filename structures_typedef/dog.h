48acfeceb273c2e137f366012a7b8285696b6f34
hentype85 <martinleiro9@gmail.com>
2023-03-20 06:23:13 -0700

    dogh

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - a dog basic info
+ * @name: first member
+ * @age: second member
+ * @owner: third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
