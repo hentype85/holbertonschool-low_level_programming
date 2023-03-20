fd790772864555e874ba2ad5f153ef495d2b2a99
hentype85 <martinleiro9@gmail.com>
2023-03-20 10:37:07 -0700

    printDog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,11 +14,11 @@ void print_dog(struct dog *d)
 		else
 			printf("Name: (nil)\n");
 
-		printf("Age: %f\n",(*d).age);
+		printf("Age: %f\n", (*d).age);
 
 		if ((*d).owner)
 			printf("Owner: %s\n", (*d).owner);
 		else
-			printf("Owner: (nill)\n");
+			printf("Owner: (nil)\n");
 	}
 }
