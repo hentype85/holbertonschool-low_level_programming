adcd199c7eb324035517628e7299e6c9602d5d1d
hentype85 <martinleiro9@gmail.com>
2023-03-20 15:27:33 -0700

    newDog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -43,22 +43,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
 
-	if (name == NULL || age < 0 || owner == NULL)
-		return (NULL);
-
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
 
 	(*d).name = malloc(sizeof(char) * (_strlen(name) + 1));
-	(*d).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-
 	if ((*d).name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
 
+        (*d).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
 	if ((*d).owner == NULL)
 	{
 		free((*d).name);
