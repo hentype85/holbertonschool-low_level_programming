b5c85342173c60ccc9b693a47ec8e2cb33098437
hentype85 <martinleiro9@gmail.com>
2023-03-20 15:30:08 -0700

    newDog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -30,6 +30,8 @@ char *_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
+
 	return (dest);
 }
 /**
@@ -48,13 +50,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	(*d).name = malloc(sizeof(char) * (_strlen(name) + 1));
+	(*d).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+
 	if ((*d).name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
-
-        (*d).owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	
 	if ((*d).owner == NULL)
 	{
 		free((*d).name);
