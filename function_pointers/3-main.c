d881e38cb1051ddb79f8ab07657bd93f55844ef8
hentype85 <martinleiro9@gmail.com>
2023-03-22 17:30:36 -0700

    calculator

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * main - get arguments
+ * @argc: count arguments
+ * @argv: vector arguments
+ * Return: (0)
+ */
+int main(int argc, char *argv[])
+{
+	int a = 0, b = 0;
+	int (*operation)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (argv[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	operation = get_op_func(argv[2]);
+
+	printf("%d\n", operation(a, b));
+
+	return (0);
+}
