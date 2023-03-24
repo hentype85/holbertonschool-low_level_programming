b31f49a40b92f5ce41d74a75a58f5de9537a403c
hentype85 <martinleiro9@gmail.com>
2023-03-24 12:49:16 -0700

    printAll

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,5 +6,42 @@
  */
 void print_all(const char * const format, ...)
 {
+	int i = 0, flag = 0;
+	char *s;
+	va_list vlist;
 
+	va_start(vlist, format);
+
+	while (format[i])
+	{
+		flag = 0;
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(vlist, int));
+				break;
+			case 'i':
+				printf("%i", va_arg(vlist, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(vlist, double));
+				break;
+			case 's':
+				s = va_arg(vlist, char*);
+				s = (s == NULL) ? "(nil)" : s;
+				printf("%s", s);
+				break;
+			default:
+				flag = 1;
+				break;
+		}
+
+		i++;
+		if (format[i] && flag == 0)
+			printf(", ");
+
+	}
+
+	va_end(vlist);
+	printf("\n");
 }
