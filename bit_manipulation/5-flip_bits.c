d9baedf5a30671ec85801131ed618474944e5454
hentype85 <martinleiro9@gmail.com>
2023-04-10 15:30:22 -0700

    flipbits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,23 +1,24 @@
 #include "main.h"
 
 /**
- * flip_bits - returns the number of bits you would need to flip 
- * 	to get from on enumber to another
+ * flip_bits - returns the number of bits you
+ * would need to flip to get from on enumber to another
  * @n: number one
  * @m: number two
+ * Return: number of bits
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int bits = 0;
+	unsigned int nBit = 0;
 
 	while (n || m)
 	{
-		if (( n & 1) != (m & 1))
-			bits++;
+		if ((n & 1) != (m & 1))
+			nBit++;
 
 		n >>= 1;
 		m >>= 1;
 	}
 
-	return bits;
+	return (nBit);
 }
