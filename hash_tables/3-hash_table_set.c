1046ede94f0cc2ccb25046d738afd324198a0d2d
hentype85 <martinleiro9@gmail.com>
2023-04-13 17:48:03 -0700

    hashtableset

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int i = 0;
 	char *valueCopy = strdup(value);
 
-	if (ht == NULL || key == NULL || valueCopy == NULL)
+	if (ht == NULL || key == NULL || value == NULL || *key == '\0')
 		return (0);
 
 	i = key_index((const unsigned char *)key, ht->size);
